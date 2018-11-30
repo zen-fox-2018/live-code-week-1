66885b3e8b82071408cb4c5530d6a9db56255a62
Theresia Coanata <theresiacoa@DESKTOP-K5BVEFS.localdomain>
2018-11-30 11:32:36 +0700

    theresiacoa

--- easy/polynominal.js
+++ easy/polynominal.js
@@ -1,3 +1,65 @@
 function polynominal(str) {
-  //your code here
+  str = str.split('+');
+  // console.log(str);
+
+  //split angka
+  var arr1 = [];
+  var arr2 = [];
+  var angka = '0123456789';
+  for (let i = 0; i < str.length; i++) {
+    let arrAngka = [];
+    let arrHuruf = [];
+    for (let j = 0; j < str[i].length; j++) {
+      let isAngka = false;
+      for (let k = 0; k < angka.length; k++) {
+        if (str[i][j] === angka[k]) {
+          arrAngka.push(str[i][j]);
+          isAngka = true;
+        }
+      }
+      if (!isAngka) {
+        arrHuruf.push(str[i][j]);
+        //   console.log(arrHuruf);
+      }
+    }
+    arrAngka = arrAngka.join('');
+    arrHuruf = arrHuruf.join('');
+    arr1.push(arrAngka);
+    arr2.push(arrHuruf);
+  }
+  console.log(arr1, arr2);
+
+  //cari huruf yang sama
+  var result = [];
+  var calculate = 0;
+  for (let i = 0; i < arr2.length; i++) {
+    var isSame = false;
+    for (let j = i+1; j <= arr2.length; j++) {
+      // console.log(arr2[i], arr2[j]);
+      if (arr2[i] === arr2[j] && i !== j) {
+        calculate += Number(arr1[i]) + Number(arr1[j]);
+        arr2.splice(j, 1);
+  
+        isSame = true;
+      }
+    }
+    if (!isSame) {
+      calculate = Number(arr1[i]);
+    }
+    if (calculate !== 0) {
+      var combine = calculate.toString() + arr2[i];
+      result.push(combine);
+    } else if (calculate === 0) {
+      result.push(arr2[i]);
+    }
+  }
+result = result.join('+');
+return result;
 }
+
+console.log(polynominal('270a+33ab+45b+4a+3b')) // 274a+33ab+48b
+console.log(polynominal('a+2ab+3b+4c+5bc')) // a+2ab+3b+5bc+4c
+console.log(polynominal('a+2a+3a+4a+5a')) // 15a
+console.log(polynominal('a+2a+3b+4a+5a+4')) // 4+12a+3b
+console.log(polynominal('1')) // 1
+console.log(polynominal('a'))  // a
--- medium/ternak.js
+++ medium/ternak.js
@@ -1,9 +1,48 @@
 function generateBoard(str) {
-  //your code here
+  let result = [];
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === 'C') {
+      var hewan = 'Cow';
+    } else if (str[i] ===  'H') {
+      hewan = 'Horse';
+    } else if (str[i] === 'S') {
+      hewan = 'Sheep'
+    } else if (str[i] === 'P') {
+      hewan = 'Pig';
+    }
+    result.push(hewan);
+  }
+  return result;
 }
 
 function getCagesAnimals(animals) {
-  //your code here
+  // console.log(animals);
+  var ternak = [
+    { name: 'Sheep', jumlah: 1 },
+    { name: 'Horse', jumlah: 2 },
+    { name: 'Cow', jumlah: 2 },
+    { name: 'Pig', jumlah: 3 }
+  ]
+
+  var result = [];
+  for (let i = 0; i < ternak.length; i++) {
+    var count = 0;
+    var hasil = []
+    var isSame = false;
+    for (let j = 0; j < animals.length; j++) {
+      if (ternak[i].name === animals[j] && ternak[i].jumlah > count) {
+        isSame = true;
+      }
+      if (isSame) {
+        hasil.push(animals[j]);
+        count++;
+        hasil = hasil.slice(j, 1);
+      }
+    }
+    count = 0;
+    result.push(hasil);
+  }
+  return result;
 }
 
 // DRIVER CODE

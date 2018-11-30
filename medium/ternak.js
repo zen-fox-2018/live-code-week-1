17aef71503aaf00e374f9acd6890db3d09ebbd1a
maitreya <bluespider529@gmail.com>
2018-11-30 11:31:21 +0700

    live code 1

--- medium/ternak.js
+++ medium/ternak.js
@@ -1,9 +1,55 @@
+
 function generateBoard(str) {
-  //your code here
+
+  let totalLength = str.length;
+  let square = Math.sqrt(totalLength)
+  let boardCages = [];
+  let count = 0;
+
+  for(let i = 0; i < square; i++) {
+    boardCages.push([]);
+    for(let j = 0; j < square; j++) {
+      boardCages[i].push(str[count]);
+      count++;
+    }
+  }
+
+  boardCages.forEach(element => {
+    for(let i = 0; i < element.length; i++) {
+      if(element[i] === "C") {
+          element[i] = "Cow";
+        } else if (element[i] === "S") {
+          element[i] = "Sheep";
+        } else if (element[i] === "H") {
+          element[i] = "Horse";
+        } else if (element[i] === "P") {
+          element[i] = "Pig";
+      }
+    }
+  })
+  return boardCages;
 }
 
 function getCagesAnimals(animals) {
-  //your code here
+  var countC = 0;
+  let countS = 0;
+  let countH = 0;
+  let countP = 0;
+
+  for(let i = 0; i < animals.length; i++) {
+    // console.log(animals[i])
+    for(let j = 0; j < animals[i].length; j++) {
+      if (animals[i][j] === "Cow") {
+        countC++;
+      } else if (animals[i][j] === "Sheep") {
+        countS++;
+      } else if (animals[i][j] === "Horse") {
+        countH++;
+      } else if (animals[i][j] === "Pig") {
+        countP++;
+      }
+    }
+  }
 }
 
 // DRIVER CODE
@@ -24,4 +70,16 @@ console.log(getCagesAnimals(generateBoard('CCCCHHHHPPPPSSSS')))
   [ 'Sheep' ],
   [ 'Sheep' ]
 ]
+
+  - 'C' = 'Sapi' atau 'Cow'
+  - 'D' = 'Domba' atau 'Sheep'
+  - 'H' = 'Kuda' atau 'Horse'
+  - 'P' = 'Babi' atau 'Pig'
+
+  - Jumlah maksimum hewan berjenis 'babi' didalam kandang ialah 3, lebih dari itu harus diletakan pada kandang yang baru atau array baru
+  - Jumlah maksimum hewan berjenis 'kuda' didalam kandang ialah 2, lebih dari itu harus diletakan pada kandang yang baru atau array baru
+  - Jumlah maksimum hewan berjenis 'sapi' didalam kandang ialah 2, lebih dari itu harus diletakan pada kandang yang baru atau array baru
+  - Jumlah maksimum hewan berjenis 'domba' didalam kandang ialah 1, lebih dari itu harus diletakan pada kandang yang baru atau array baru
+
+  
 */

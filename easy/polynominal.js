bab915f759412da3bb8b11eb0048df4b4c61fe8b
ChristianHS23 <christian.sihotang23@gmail.com>
2018-11-30 11:30:48 +0700

    Add LiveCode Week 1 .js

--- easy/polynominal.js
+++ easy/polynominal.js
@@ -1,3 +1,31 @@
 function polynominal(str) {
   //your code here
+   strArray = str.split('+')
+   console.log(strArray)
+   let output = []
+   for(let i =0; i < strArray.length; i++){
+    var outputDalam=[]
+    let tempAngka=''
+    let tempVariabel=''
+    for(let j = 0; j < strArray[i].length; j++){
+      if(!Number(strArray[i][j]) && strArray[i][j] !=0){
+        tempVariabel += strArray[i][j]
+      }else {
+        tempAngka += strArray[i][j]
+      }
+      }   
+      outputDalam.push(tempAngka,tempVariabel)
+    output.push(outputDalam)
+   }
+   for(let i = 0 ;i<output.length;i++){
+  
+   }
+   return output
 }
+
+console.log(polynominal('270a+33ab+45b+4a+3b')) // 274a+33ab+48b
+// console.log(polynominal('a+2ab+3b+4c+5bc')) // a+2ab+3b+5bc+4c
+// console.log(polynominal('a+2a+3a+4a+5a')) // 15a
+// console.log(polynominal('a+2a+3b+4a+5a+4')) // 4+12a+3b
+// console.log(polynominal('1')) // 1
+// console.log(polynominal('a'))  // a

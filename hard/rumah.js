3d0071ecef94123ee6732c51c4a1deebdcb1d167
rahmanhasri <rahman.hasri@gmail.com>
2018-11-30 11:32:28 +0700

    maman medium dan hard belum

--- hard/rumah.js
+++ hard/rumah.js
@@ -1,5 +1,36 @@
 function generateHouse (size, heightDoor, widthDoor) {
   //your code here
+  let string = ''
+  let lebar = (size * 2) - 1
+  for (let i = 0 ; i < size * 2 -1 ; i++) {
+    for (let j = 0 ; j < lebar ; j++) {
+      if (i <= 4){
+        if(i === 4 || j === lebar - size - i || j === size + i - 1){
+          string += '*'
+        } else   {
+          string += ' '
+        }
+      } else if (i > 4) {
+        if (i === lebar - 1 && j > 0 && j < lebar - 1){
+          if (j > 2 && j < 6){
+            string += '#'
+          } else {
+            string += '*'
+          }
+        } else if (i > 5 && j > 2 && j < 6){
+          // if (i > 6 && i < )
+          string += '#'
+          // j === 1 || j === lebar - 2){
+        } else if (j === 1 || j === lebar - 2) {
+          string += '*'
+        } else {
+          string += ' '
+        }
+      }
+    }
+    string += '\n'
+  }
+  console.log(string);
 }
 
 generateHouse(5, 3, 3)
@@ -14,7 +45,7 @@ generateHouse(5, 3, 3)
   *   #   #   *
   * * # # # * *
 */
-generateHouse(6, 4, 3)
+// generateHouse(6, 4, 3)
 /* OUTPUT
           *
         *   *
--- medium/ternak.js
+++ medium/ternak.js
@@ -1,12 +1,77 @@
 function generateBoard(str) {
   //your code here
+  let output = []
+  let size = str.length
+
+  for (let i = 1 ; i < size ; i++){
+    if (i * i === size){
+      size = i
+      break;
+    }
+  }
+  let kamus = { C : 'Cow', H : 'Horse', S : 'Sheep', P : 'Pig'}
+
+  let newKandang = []
+  for (let i = 0 ; i < str.length ; i++) {
+    if (newKandang.length === size){
+      output.push(newKandang)
+      newKandang = []
+    }
+    if (kamus[str[i]] !== undefined) {
+      newKandang.push(kamus[str[i]])
+    }
+  }
+  if (newKandang.length > 0){
+    output.push(newKandang)
+  }
+
+  return output
 }
+
 
 function getCagesAnimals(animals) {
   //your code here
+  let binatang = []
+  let objBinatang = {}
+  let kamus = { 'Cow' : 2, 'Horse' : 2, 'Sheep' : 1, 'Pig' : 3}
+
+  for (let i = 0 ; i < animals.length ; i++) {
+    for (let j = 0 ; j < animals[i].length ; j++) {
+      binatang.push(animals[i][j])
+    }
+  }
+
+  for (let i = 0 ; i < animals.length ; i++) {
+    for (let j = 0 ; j < animals[i].length ; j++) {
+      if (objBinatang[animals[i][j]] === undefined) {
+        objBinatang[animals[i][j]] = 1
+      } else {
+        objBinatang[animals[i][j]]++
+      }
+    }
+  }
+
+  let output = []
+  let arrayBin = Object.keys(kamus)
+  let hitungLimit = []
+  let counter = {}
+  for (let key in kamus) {
+    hitungLimit.push(kamus[key])
+    counter[key] = 0
+  }
+
+  return counter
 }
 
 // DRIVER CODE
+/* OUTPUT
+[
+  [ 'Cow', 'Horse', 'Cow', 'Pig' ],
+  [ 'Sheep', 'Cow', 'Sheep', 'Horse' ],
+  [ 'Horse', 'Pig', 'Sheep', 'Horse' ],
+  [ 'Cow', 'Pig', 'Pig', 'Horse' ]
+]
+*/
 
 console.log(getCagesAnimals(generateBoard('CHCPSCSHHPSHCPPH')))
 /* OUTPUT
@@ -16,7 +81,7 @@ console.log(getCagesAnimals(generateBoard('CHCPSCSHHPSHCPPH')))
   [ 'Sheep', 'Horse' ]
 ]
 */
-console.log(getCagesAnimals(generateBoard('CCCCHHHHPPPPSSSS')))
+// console.log(getCagesAnimals(generateBoard('CCCCHHHHPPPPSSSS')))
 /* OUTPUT
 [
   [ 'Cow', 'Cow', 'Horse', 'Horse', 'Pig', 'Pig', 'Pig', 'Sheep' ],

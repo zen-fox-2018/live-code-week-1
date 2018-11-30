a5457fec804c6cc3ebc1e9318dfc3f1109a72a64
fauzan_bintang <obin.aji@gmail.com>
2018-11-30 11:30:34 +0700

    done

--- hard/rumah.js
+++ hard/rumah.js
@@ -1,5 +1,38 @@
 function generateHouse (size, heightDoor, widthDoor) {
-  //your code here
+  if (size < 5 || widthDoor < 3 || heightDoor < 3) {
+    return 'Rumah tidak bisa dibuat'
+  } else {
+    let height = (size*2) - 1
+    let mid = Math.floor(height/2)
+    let left = mid;
+    let right = mid;
+    for (let i = 0; i < height; i++) {
+      let row = ''
+      for (let j = 0; j < height; j++) {
+        if (i === 0 && j === mid) {
+          row += '*'
+        } else if (j === left || j === right ) {
+          row += '*'
+        } else if (i === size - 1) {
+          row += '*'
+        } else if (j === 1 && i > size -1 || j === height - 2 && i > size -1) {
+          row += '*'
+        } else if (i === height - 1 && j > 1 && j < mid - ((widthDoor-1) / 2) || i === height - 1 && j < height-2 && j > mid + ((widthDoor-1) / 2)) {
+          row += '*'
+        } else if (i >= height - heightDoor && j === mid - ((widthDoor-1) / 2) || i >= height - heightDoor && j === mid + ((widthDoor-1) / 2)) {
+          row += '#'
+        } else if (j === mid && i === height - heightDoor || j === mid && i === height -1) {
+          row += '#'
+        }
+        else {
+          row += ' '
+        }
+      }
+      left --
+      right ++
+      console.log(row);
+    }
+  }
 }
 
 generateHouse(5, 3, 3)
@@ -14,7 +47,7 @@ generateHouse(5, 3, 3)
   *   #   #   *
   * * # # # * *
 */
-generateHouse(6, 4, 3)
+generateHouse(7, 4, 3)
 /* OUTPUT
           *
         *   *

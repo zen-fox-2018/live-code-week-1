e98ffbfb08ad2329043ae11a8b06fe8cb835e363
rachmadza <razato.zai@gmail.com>
2018-11-30 11:32:01 +0700

    push live code week 1

--- hard/rumah.js
+++ hard/rumah.js
@@ -1,5 +1,51 @@
 function generateHouse (size, heightDoor, widthDoor) {
   //your code here
+
+  if (size < 5 || heightDoor < 3 || widthDoor < 3) {
+     return 'Rumah tidak akan bisa dibuat'
+  }
+  
+
+  for (let i = 0; i < size; i++) {
+    let atap = ''
+    for (let j = 0; j < size+size; j++) {
+      if (i==0 && j == 4) {
+        atap += '*'
+      } else {
+        atap += ' '
+        // console.log(atap)
+      }
+
+      if (i==1 && (j==3 || j == 5)) {
+        atap += '*'
+      } else {
+        atap += ' '
+      }
+
+      if (i==2 && (j==2 || j == 6)) {
+        atap += '*'
+      } else {
+        atap += ' '
+      }
+
+      if (i==3 && (j==1 || j == 7)) {
+        atap += '*'
+      } else {
+        atap += ' '
+      }
+
+      if (i==4 && (j==0 || j == 8)) {
+        atap += '*'
+      } else {
+        atap += ' '
+      }
+
+      
+    }
+    
+    console.log(atap)
+  }
+
 }
 
 generateHouse(5, 3, 3)
@@ -14,7 +60,7 @@ generateHouse(5, 3, 3)
   *   #   #   *
   * * # # # * *
 */
-generateHouse(6, 4, 3)
+// generateHouse(6, 4, 3)
 /* OUTPUT
           *
         *   *

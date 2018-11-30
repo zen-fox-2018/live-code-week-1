30db73e74771561c77f424bdad002f1218f62d4a
Mahdihrs <mahdiharis@outlook.com>
2018-11-30 11:31:51 +0700

    Mahdi Haris Live Code 1

--- easy/polynominal.js
+++ easy/polynominal.js
@@ -1,3 +1,101 @@
 function polynominal(str) {
-  //your code here
+  let strArr = str.split('+')
+  let objKoefisien = generateUnik(strArr)
+  let keysObjKoefisien = Object.keys(objKoefisien)
+  let valuesObjKoefisien = Object.values(objKoefisien)
+
+  for (let i = 0; i < strArr.length; i++) {
+    let koefisien = ''
+    let num = 0
+    //new
+    // let numStr = ''
+    for (let j = 0; j < strArr[i].length; j++) {
+      if (cekKoefisien(strArr[i][j])) {
+        koefisien += strArr[i][j]
+      }
+      else if (cekNum(strArr[i][j])) {
+        num += Number(strArr[i][j])
+      }
+    } 
+    for (let k = 0; k < keysObjKoefisien.length; k++) {
+      if (koefisien.length === 0 && keysObjKoefisien[k].length === 0) {
+        valuesObjKoefisien[k] += num
+      }
+      else if (keysObjKoefisien[k] === koefisien) {
+        if (num === 0) {
+          valuesObjKoefisien[k] += 1
+        }
+        else {
+          valuesObjKoefisien[k] += num
+        }
+      }
+    }
+  }
+  // console.log(keysObjKoefisien, valuesObjKoefisien);
+  
+  let finalResult = mergeResult(keysObjKoefisien, valuesObjKoefisien)
+  return finalResult
 }
+
+function mergeResult(keys, values) {
+  let result = []
+  for (let i = 0; i < keys.length; i++) {
+    let angka = ''
+    if (keys[i].length === 0) {
+      angka = values[i]
+    }
+    else if (values[i] === 1) {
+      angka = keys[i]
+    }
+    else {
+      angka = values[i]
+      angka += keys[i]
+    }
+    result.push(angka)
+  }
+  return result.join('+')
+}
+
+function cekKoefisien(kons) {
+  let koefisien = 'abcdefghijklmnopqrstuvwxyz'
+  for (let i = 0; i < koefisien.length; i++) {
+    if (kons === koefisien[i]) {
+      return true
+    }
+  }
+  return false
+}
+
+function cekNum(num) {
+  let numList = '0123456789'
+  for (let i = 0; i < numList.length; i++) {
+    if (num === numList[i]) {
+      return true
+    }
+  }
+  return false
+}
+
+function generateUnik(data) {
+  let obj = {}
+  for (let i = 0; i < data.length; i++) {
+    let koefisienGabung = ''
+    for (let j = 0; j < data[i].length; j++) {
+      if (cekKoefisien(data[i][j])) {
+        koefisienGabung += data[i][j]
+      }
+    }
+    if (obj[koefisienGabung] === undefined) {
+      obj[koefisienGabung] = 0
+    }
+  }
+  return obj
+}
+
+//TEST CASE
+console.log(polynominal('270a+33ab+45b+4a+3b')) // 274a+33ab+48b //belum selesai
+console.log(polynominal('a+2ab+3b+4c+5bc')) // a+2ab+3b+5bc+4c
+console.log(polynominal('a+2a+3a+4a+5a')) // 15a
+console.log(polynominal('a+2a+3b+4a+5a+4')) // 4+12a+3b
+console.log(polynominal('1')) // 1
+console.log(polynominal('a'))  // a
--- hard/rumah.js
+++ hard/rumah.js
@@ -1,5 +1,50 @@
 function generateHouse (size, heightDoor, widthDoor) {
-  //your code here
+  if (size < 5 || heightDoor < 3 || widthDoor < 3) {
+    console.log('Rumah tidak bisa dibuat');
+  }
+  //atap
+  console.log(atap(size))
+}
+
+function atap(size) {
+  let hasil = []
+  let col = (size * 2) - 1
+  let startLeftDiagonal = Math.floor(col / 2)
+  let startRightDiagonal = Math.floor(col / 2)
+  for (let i = 0; i < size; i++) {
+    let atap = []
+    for (let j = 0; j < col; j++) {
+      if (i === 0 && j === Math.floor(col / 2)) {
+        atap.push('*')
+      }
+      else if (i !== 0 && i < size - 1 && j === startLeftDiagonal) {
+        atap.push('* ')
+        // startLeftDiagonal--
+      }
+      else if (i !== 0 && i < size - 1 && j === startRightDiagonal) {
+        atap.push('* ')
+      }
+      else if (i === size - 1) {
+        atap.push('* ')
+      }
+      else {
+        atap.push('  ')
+      }
+    }
+    startRightDiagonal++
+    startLeftDiagonal--
+    atap = atap.join('')
+    hasil.push(atap)
+  }
+  hasil = hasil.join('\n')
+  return hasil
+}
+
+function bangunan(width, height) {
+  let hasil = []
+
+  hasil = hasil.join('\n')
+  return hasil
 }
 
 generateHouse(5, 3, 3)
@@ -14,7 +59,8 @@ generateHouse(5, 3, 3)
   *   #   #   *
   * * # # # * *
 */
-generateHouse(6, 4, 3)
+
+// generateHouse(6, 4, 3)
 /* OUTPUT
           *
         *   *
@@ -27,5 +73,23 @@ generateHouse(6, 4, 3)
   *     #   #     *
   *     #   #     *
   * * * # # # * * *
+
+*/
+
+generateHouse(7, 4, 3)
+/*
+       		  *              
+          *   *            
+        *       *          
+      *           *        
+    *               *      
+  *                   *    
+* * * * * * * * * * * * * 
+  *                   *   
+  *                   *   
+  *       # # #       *   
+  *       #   #       *   
+  *       #   #       *   
+  * * * * # # # * * * *   
 
 */

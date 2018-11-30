519feb642c38574ce15103eed11993b74e5f003c
kangman53 <hermanboy53@gmail.com>
2018-11-30 11:31:07 +0700

    live code week 1

--- hard/rumah.js
+++ hard/rumah.js
@@ -1,4 +1,51 @@
 function generateHouse (size, heightDoor, widthDoor) {
+  let height = 2 * size - 1;
+  let myHouse = '';
+  for (let i = 1; i <= height; i++) {
+    let tempHouse = '';
+    for (var j = 1; j <= height; j++) {
+      if (i < size) {
+        if (j === size - (i - 1) || j === size + (i - 1)) {
+          tempHouse += '* ';
+        } else {
+          tempHouse += '  ';
+        }
+      } else if (i === size) {
+        tempHouse += '* ';
+      } else if (i === height){
+        if ( j === 1 || j === height){
+          tempHouse += '  ';
+        } else if (j <= Math.floor((height - widthDoor) / 2) || j > Math.floor((height - widthDoor) / 2) + widthDoor) {
+          tempHouse += '* ';
+        } else {
+          tempHouse += '# ';
+        }
+      } else if (i <= height - heightDoor) {
+        if(j === 2 || j === height - 1) {
+          tempHouse += '* ';
+        } else {
+          tempHouse += '  ';
+        }
+      } else if (i === height - heightDoor + 1) {
+        if (j === 2 || j === height - 1 ){
+          tempHouse += '* ';
+        } else if (j >= Math.floor((height - widthDoor) / 2) + 1 && j <= Math.floor((height - widthDoor) / 2) + widthDoor) {
+          tempHouse += '# ';
+        } else {
+          tempHouse += '  ';
+        }
+      } else {
+        if (j === 2 || j === height - 1 ){
+          tempHouse += '* ';
+        } else if (j === Math.floor((height - widthDoor) / 2) + 1 || j === Math.floor((height - widthDoor) / 2) + widthDoor) {
+          tempHouse += '# ';
+        } else {
+          tempHouse += '  ';
+        }
+      }
+    }
+    console.log(tempHouse);
+  }
   //your code here
 }
 
--- medium/ternak.js
+++ medium/ternak.js
@@ -1,9 +1,56 @@
 function generateBoard(str) {
+  if(str.length !== Math.pow(Math.sqrt(str.length), 2)) {
+    return false;
+  }
+  let myBoard = [];
+  let tempBoard = [];
+  let objAnimals = {
+    'P' : 'Pig',
+    'H' : 'Horse',
+    'C' : 'Cow',
+    'S' : 'Sheep'
+  };
+  let maxBoard = Math.sqrt(str.length);
+  for (e of str) {
+    tempBoard.push(objAnimals[e]);
+    if(tempBoard.length === maxBoard){
+      myBoard.push(tempBoard);
+      tempBoard = [];
+    }
+  }
+
+  return myBoard;
   //your code here
 }
 
 function getCagesAnimals(animals) {
   //your code here
+  let objAnimals = {
+    'Pig' : 3,
+    'Horse' : 2,
+    'Cow' : 2,
+    'Sheep' : 1
+  };
+  let result = [];
+  animals.forEach( hewans => {
+    hewans.forEach( hewan => {
+      if(!result.length) {
+        result.push([]);
+      }
+      for (let i = 0; i < result.length; i++) {
+        let max = result[i].filter(f => f === hewan).length;
+        if(max < objAnimals[hewan]) {
+          result[i].push(hewan);
+          break;
+        } else if(result[i + 1] != undefined){
+          continue;
+        } else {
+          result.push([]);
+        }
+      }
+    });
+  });
+  return result;
 }
 
 // DRIVER CODE

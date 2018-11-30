51f597463e70b1478aef23fc5ab7799036c7fbb0
Celyn Susanto <celyn.vy@gmail.com>
2018-11-30 11:30:57 +0700

    live code 1

--- easy/polynominal.js
+++ easy/polynominal.js
@@ -1,3 +1,55 @@
 function polynominal(str) {
   //your code here
+  let strSplit = str.split("+");
+  // console.log(strSplit);
+  let object = {};
+  for (let i = 0; i <= strSplit.length-1; i++) {
+    var tempAngka = ""
+    var tempHuruf = ""
+    for (let j = 0; j <= strSplit[i].length-1; j++) {
+      if (checkAngka(strSplit[i][j]) === true) {
+        tempAngka += strSplit[i][j];
+      } else {
+        tempHuruf += strSplit[i][j];
+      }
+    }
+    if (object[tempHuruf] === undefined) {
+      object[tempHuruf] = tempAngka; 
+    } else {
+      if ( object[tempHuruf].length === 0) {
+        object[tempHuruf] = 1 + Number(tempAngka);
+      } else {
+        object[tempHuruf] = Number(object[tempHuruf]) + Number(tempAngka);
+      }
+    }
+  }
+
+  let result = "";
+  let keys = Object.keys(object);
+  let values = Object.values(object);
+
+  for (let i = 0; i <= keys.length-1; i++) {
+    if (i !== 0) {
+      result += "+";
+    }
+    result += (values[i] + keys[i]);
+  }
+  return result;
 }
+function checkAngka(str) {
+  let angka = "0123456789";
+
+  for (let i = 0; i <= angka.length-1; i++) {
+    if (angka[i] === str) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(polynominal('270a+33ab+45b+4a+3b')) // 274a+33ab+48b
+console.log(polynominal('a+2ab+3b+4c+5bc')) // a+2ab+3b+5bc+4c
+console.log(polynominal('a+2a+3a+4a+5a')) // 15a
+console.log(polynominal('a+2a+3b+4a+5a+4')) // 4+12a+3b
+console.log(polynominal('1')) // 1
+console.log(polynominal('a'))  // a
--- medium/ternak.js
+++ medium/ternak.js
@@ -1,9 +1,60 @@
+// - 'C' = 'Sapi' atau 'Cow'
+// - 'D' = 'Domba' atau 'Sheep'
+// - 'H' = 'Kuda' atau 'Horse'
+// - 'P' = 'Babi' atau 'Pig'
+
+
 function generateBoard(str) {
   //your code here
+  let list = {C : "Cow", S : "Sheep", H : "Horse", P : "Pig"};
+   
+  let result = [];
+  let j = 0;
+  while (j <= str.length-1) {
+    let temp = [];
+    for (let i = 0; i <= Math.sqrt(str.length); i++) {
+      if (str[j] !== undefined) {
+        temp.push(list[str[j]]);
+        getCagesAnimals
+        j++;
+      } 
+    }
+    result.push(temp);
+  }
+  return getCagesAnimals(result)
 }
 
 function getCagesAnimals(animals) {
-  //your code here
+  // your code here
+  for (let i = 0; i <= animals.length-1; i++) {
+    let list = {Cow : 2, Sheep : 1, Horse: 2 ,Pig: 3};
+    let keys = Object.keys(list);
+
+    // console.log(animals[i])
+    for (let j = 0; j <= animals[i].length-1; j++) {
+      let isFull = false;
+      for (let k = 0; k <= keys.length-1; k++) {
+        if (keys[k] === animals[i][j]) {
+          // console.log(list[keys[k]] - 1)
+          list[keys[k]] =  list[keys[k]] - 1;
+          if (list[keys[k]] === 0) {
+            isFull = true
+            break;
+          }
+        }
+      }
+      if (isFull === true) {
+        animals[i].splice(j, j+1);
+        if (animals[i+1] != undefined) {
+          animals[i+1].push(animals[i][j])
+        } else {
+          animals.push([])
+          animals[i+1].push(animals[i][j])
+        }
+      }
+    }
+  }
+  return animals
 }
 
 // DRIVER CODE

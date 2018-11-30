7d708f5f9c72c6a3051607ed82a7a994d8755bc4
ACER <fauziaazia171097@gmail.com>
2018-11-29 23:32:00 -0500

    Fauzia_Nabilah Rahmawati

--- medium/ternak.js
+++ medium/ternak.js
@@ -1,10 +1,57 @@
 function generateBoard(str) {
+  let data = str.split("")
+  return data
   //your code here
 }
 
 function getCagesAnimals(animals) {
-  //your code here
+  // console.log(animals)
+  let ternak = [
+    {
+      huruf: "C",
+      jenis : "Cow",
+      batas: 2
+    },
+    {
+      huruf:"H",
+      jenis:"Horse",
+      batas: 2 ,
+    },
+    {
+      huruf:"S",
+      jenis: "Sheep",
+      batas: 1,
+    },
+    {
+      huruf: "P",
+      jenis: "Pig",
+      batas: 3
+    }
+  ]
+  let kandang = [[]]
+  animals.forEach( hewan => {
+    ternak.forEach(x => {
+      // console.log(kandang[kandang.length-1])
+      
+      if(hewan === x.huruf) {
+        for( let i = 0 ; i < kandang.length ; i++) {
+          let batas = 0
+          kandang[i] .forEach(isi=>{
+            if (isi === x.jenis) {
+              batas++
+            }
+          })
+          if(batas < x.batas) {
+            kandang[i].push(x.jenis)
+          }
+        }
+      }
+    })
+    
+  });
+  return kandang
 }
+  //your code here
 
 // DRIVER CODE
 
